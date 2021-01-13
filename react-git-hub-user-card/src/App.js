92f9813744df1e38675b219c2ec9395c8f1b6e02
RubesWorld <hi@rubesworld.live>
2021-01-12 17:55:46 -0800

    Completed Functionality

--- react-git-hub-user-card/src/App.js
+++ react-git-hub-user-card/src/App.js
@@ -3,19 +3,18 @@ import React from 'react'
 import './App.css';
 
 //Component Imports
-import UserCard from './UserCard';
+import FollowerCard from './FollowerCard';
 
 
 const info = [
-  {
-    name: "",
-    bio: "",
-    followers: "",
-    following: "",
-    location: ""
-  }
+    {
+      name: "",
+      bio: "",
+      followers: "",
+      following: "",
+      location: ""
+    }
 ]
-
 
 class App extends React.Component {
   state = {
@@ -40,8 +39,16 @@ class App extends React.Component {
     return (
       <>
           <div className="main-card">
-            <h1>GitHub UserCard!</h1>
-            <UserCard info={this.state.user}/>
+              <div className="top-half">
+                <h1>GitHub UserCard!</h1>
+                <div className="img-container">
+                  <img  src={this.state.user["avatar.url"]} alt="github image" width="200" height="200"/>
+                </div>
+                <h2>{this.state.user.login}</h2>
+                <h2>{this.state.user.location}</h2>
+                <p>{this.state.user.bio}</p>
+              </div>
+            <FollowerCard/>
           </div>
     </> 
   )
